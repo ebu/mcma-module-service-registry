1c2bebaedebad818ba42c16e2245a3bded9845d6
Joost Rovers <joost@rovers.pt>
2024-09-25 13:39:27 +0100

    Changed version tags to have prefix "v"

--- deployment/terraform.tf
+++ deployment/terraform.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.22.0"
-    }
     mcma = {
       source  = "ebu/mcma"
       version = ">= 0.0.27"
